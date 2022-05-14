1097af6a2829010d44d1a2e63fe2430da2632b71
roylin <roylin@ryzo.io>
2022-05-14 22:41:18 +0800

    更新

--- handlers/ronin.js
+++ handlers/ronin.js
@@ -3,11 +3,13 @@ const got = require('got')
 const bip39 = require('bip39')
 const { hdkey } = require('ethereumjs-wallet')
 const dayjs = require('dayjs')
+const async = require('async')
 
 const SLP_CONTRACT = '0xa8754b9fa15fc18bb59458815510e40a12cd2014'
 const AXS_CONTRACT = '0x97a9107c1793bc407d6f527b77e7fff4d812bece'
 const AXIE_CONTRACT = '0x32950db2a7164ae833121501c797d79e7b79d74c'
 const WETH_CONTRACT = '0xc99a6a985ed2cac1ef41640596c5a5f9f4e19ef5'
+const RON_CONTRACT = '0xe514d9deb7966c8be0ca922de8a064264ea6bcd4'
 const RONIN_PROVIDER_FREE = 'https://proxy.roninchain.com/free-gas-rpc'
 const RONIN_PROVIDER = 'https://api.roninchain.com/rpc'
 const abi = [
@@ -96,6 +98,10 @@ const abi = [
   }
 ]
 
+const sleep = (time) => {
+  return new Promise((resolve) => setTimeout(resolve, time))
+}
+
 exports.get_key = (mnemonic, index) => {
   const seed = bip39.mnemonicToSeedSync(mnemonic)
   const hdwallet = hdkey.fromMasterSeed(seed)
@@ -107,23 +113,42 @@ exports.get_key = (mnemonic, index) => {
 }
 
 const get_balance = async (address, token = 'slp') => {
-  let contract_address = ''
-  if (token === 'slp') {
-    contract_address = SLP_CONTRACT
-  } else if (token === 'axs') {
-    contract_address = AXS_CONTRACT
-  } else if (token === 'axie') {
-    contract_address = AXIE_CONTRACT
-  } else if (token === 'weth') {
-    contract_address = WETH_CONTRACT
-  }
-  const web3 = new Web3(new Web3.providers.HttpProvider(RONIN_PROVIDER))
-  const contract = new web3.eth.Contract(abi, web3.utils.toChecksumAddress(contract_address))
-  let balance = await contract.methods.balanceOf(web3.utils.toChecksumAddress(address)).call()
-  if (token === 'weth') {
-    balance = Math.round(web3.utils.fromWei(balance, 'ether') * 1000) / 1000
-  }
-  return Number(balance)
+  const result = await async
+    .forever(async () => {
+      let contract_address = ''
+      if (token === 'slp') {
+        contract_address = SLP_CONTRACT
+      } else if (token === 'axs') {
+        contract_address = AXS_CONTRACT
+      } else if (token === 'axie') {
+        contract_address = AXIE_CONTRACT
+      } else if (token === 'weth') {
+        contract_address = WETH_CONTRACT
+      } else if (token === 'ron') {
+        contract_address = RON_CONTRACT
+      }
+      const web3 = new Web3(new Web3.providers.HttpProvider(RONIN_PROVIDER))
+      const contract = new web3.eth.Contract(abi, web3.utils.toChecksumAddress(contract_address))
+      let balance = await contract.methods
+        .balanceOf(web3.utils.toChecksumAddress(address))
+        .call()
+        .catch((err) => {
+          console.log(err.message)
+          return { err: true }
+        })
+      if (token === 'weth') {
+        balance = Math.round(web3.utils.fromWei(balance, 'ether') * 1000) / 1000
+      }
+      if (balance && !balance.err) {
+        return Promise.reject(balance)
+      }
+      console.log('sleep 200')
+      await sleep(200)
+    })
+    .catch((result) => {
+      return result.message
+    })
+  return Number(result)
 }
 exports.get_balance = get_balance
 
@@ -134,14 +159,31 @@ exports.has_claimable_slp = async (address) => {
     responseType: 'json',
     resolveBodyOnly: true
   }
-  const slp_in_game = await got(options)
+  const slp_in_game = await got(options).catch((err) => {
+    console.log('log2', err)
+    return Promise.reject(err)
+  })
   // console.log(slp_in_game)
-  const slp_in_wallet = await get_balance(address, 'slp')
-  if (dayjs.unix(slp_in_game.last_claimed_item_at).add(14, 'day').isAfter(dayjs())) {
-    return 0
-  }
-  if (slp_in_wallet === 0 && slp_in_game.raw_total > slp_in_game.raw_claimable_total) {
+  const slp_in_wallet = await get_balance(address, 'slp').catch((err) => {
+    console.log('log1', err)
+    return Promise.reject(err)
+  })
+  // if (dayjs.unix(slp_in_game.last_claimed_item_at).add(14, 'day').isAfter(dayjs())) {
+  //   return 0
+  // }
+  if (
+    dayjs.unix(slp_in_game.last_claimed_item_at).add(14, 'day').isBefore(dayjs()) &&
+    slp_in_wallet === 0 &&
+    slp_in_game.raw_total > slp_in_game.raw_claimable_total
+  ) {
     return slp_in_game.raw_total - slp_in_game.raw_claimable_total
+  }
+  if (
+    slp_in_wallet === 0 &&
+    slp_in_game.blockchain_related.signature &&
+    slp_in_game.blockchain_related.signature.amount > slp_in_game.blockchain_related.checkpoint
+  ) {
+    return slp_in_game.blockchain_related.signature.amount - slp_in_game.blockchain_related.checkpoint
   }
   return 0
   // {
@@ -291,25 +333,41 @@ const apply_claim = async (address, private_key) => {
 exports.apply_claim = apply_claim
 
 exports.send_claim = async (address, private_key) => {
-  const signature = await apply_claim(address, private_key)
-  const web3 = new Web3(new Web3.providers.HttpProvider(RONIN_PROVIDER_FREE))
-  const contract = new web3.eth.Contract(abi, web3.utils.toChecksumAddress(SLP_CONTRACT))
-  const nonce = await get_nonce(address)
-  const encodeABI = await contract.methods
-    .checkpoint(web3.utils.toChecksumAddress(address), signature.amount, signature.timestamp, signature.signature)
-    .encodeABI()
-  const signed_claim = await web3.eth.accounts.signTransaction(
-    {
-      nonce: nonce,
-      to: web3.utils.toChecksumAddress(SLP_CONTRACT),
-      data: encodeABI,
-      gasPrice: 0,
-      gas: 492874
-    },
-    private_key
-  )
-  const send_claim = await web3.eth.sendSignedTransaction(signed_claim.rawTransaction)
-  return send_claim
+  await async
+    .forever(async () => {
+      try {
+        const signature = await apply_claim(address, private_key)
+        const web3 = new Web3(new Web3.providers.HttpProvider(RONIN_PROVIDER_FREE))
+        const contract = new web3.eth.Contract(abi, web3.utils.toChecksumAddress(SLP_CONTRACT))
+        const nonce = await get_nonce(address)
+        const encodeABI = await contract.methods
+          .checkpoint(web3.utils.toChecksumAddress(address), signature.amount, signature.timestamp, signature.signature)
+          .encodeABI()
+        const signed_claim = await web3.eth.accounts.signTransaction(
+          {
+            nonce: nonce,
+            to: web3.utils.toChecksumAddress(SLP_CONTRACT),
+            data: encodeABI,
+            gasPrice: 0,
+            gas: 492874
+          },
+          private_key
+        )
+        const send_claim = await web3.eth.sendSignedTransaction(signed_claim.rawTransaction)
+        return Promise.reject(JSON.stringify(send_claim))
+      } catch (error) {
+        console.log('log: ' + error.message)
+        if (!error.message.includes('Invalid JSON RPC response') && !error.message.includes('Failed to check')) {
+          return Promise.reject(JSON.stringify(error))
+        }
+      }
+      console.log('send_claim sleep 200')
+      await sleep(200)
+    })
+    .catch((result) => {
+      return JSON.parse(result.message)
+    })
+
   // {
   //   blockHash: '0xef58c47b6352920020eb5df9fce60d2ff513c2126fe169a3cc87de03197f4e9c',
   //   blockNumber: 9582200,
@@ -334,21 +392,36 @@ exports.get_receipt = async (hash) => {
 }
 
 exports.transfer_slp = async (from_address, private_key, to_address, amount) => {
-  const web3 = new Web3(new Web3.providers.HttpProvider(RONIN_PROVIDER_FREE))
-  const contract = new web3.eth.Contract(abi, web3.utils.toChecksumAddress(SLP_CONTRACT))
-  const nonce = await get_nonce(from_address)
-  const encodeABI = await contract.methods.transfer(web3.utils.toChecksumAddress(to_address), amount).encodeABI()
-  const signed_claim = await web3.eth.accounts.signTransaction(
-    {
-      nonce: nonce,
-      chainId: 2020,
-      to: web3.utils.toChecksumAddress(SLP_CONTRACT),
-      data: encodeABI,
-      gasPrice: 0,
-      gas: 246437
-    },
-    private_key
-  )
-  const send_claim = await web3.eth.sendSignedTransaction(signed_claim.rawTransaction)
-  return send_claim
+  await async
+    .forever(async () => {
+      try {
+        const web3 = new Web3(new Web3.providers.HttpProvider(RONIN_PROVIDER_FREE))
+        const contract = new web3.eth.Contract(abi, web3.utils.toChecksumAddress(SLP_CONTRACT))
+        const nonce = await get_nonce(from_address)
+        const encodeABI = await contract.methods.transfer(web3.utils.toChecksumAddress(to_address), amount).encodeABI()
+        const signed_claim = await web3.eth.accounts.signTransaction(
+          {
+            nonce: nonce,
+            chainId: 2020,
+            to: web3.utils.toChecksumAddress(SLP_CONTRACT),
+            data: encodeABI,
+            gasPrice: 0,
+            gas: 246437
+          },
+          private_key
+        )
+        const send_claim = await web3.eth.sendSignedTransaction(signed_claim.rawTransaction)
+        return Promise.reject(JSON.stringify(send_claim))
+      } catch (error) {
+        console.log(error.message)
+        if (error.message !== 'Invalid JSON RPC response: "free gas request times is exceeded"') {
+          return Promise.reject(JSON.stringify(error))
+        }
+      }
+      console.log('transfer_slp sleep 200')
+      await sleep(200)
+    })
+    .catch((result) => {
+      return JSON.parse(result.message)
+    })
 }
--- slp.js
+++ slp.js
@@ -9,9 +9,12 @@
 
     const mnemonic = process.env.mnemonic
     const skip = 2
-    const total = 82
+    const total = 87
     const main_wallet = ronin_handler.get_key(mnemonic, 1)
 
+    const sleep = (time) => {
+      return new Promise((resolve) => setTimeout(resolve, time))
+    }
     if (argv.job === 'csv') {
       let info = await fs.readJSON('./db/info.json')
       let scholar = await fs.readFile('./db/scholar.txt', 'utf8')
@@ -23,17 +26,30 @@
         const name = e[0]
         const extra = e[1]
         const address = e[2]
+        const reduce = Number(e[3]) || undefined
         if (!address) {
           info[index] = { i: index + 1, address: '' }
         } else if (address.includes('ronin:')) {
           info[index].name = name
-          info[index].extra = extra ? true : false
+          if (extra === 'x') {
+            info[index].energy = 40
+          } else if (extra === 'xx') {
+            info[index].energy = 60
+          } else {
+            delete info[index].energy
+          }
           info[index].address = address
+          info[index].reduce = reduce
         } else {
           const number = address.split(',')[0]
           info[index].name = name
-          info[index].extra = extra ? true : false
+          if (extra === 'x') {
+            info[index].energy = 40
+          } else if (extra === 'xx') {
+            info[index].energy = 60
+          }
           info[index].address = info[number - 1].address
+          info[index].reduce = reduce
         }
       })
       await fs.writeJSON('./db/info.json', info)
@@ -67,7 +83,7 @@
         },
         { concurrency: 3 }
       )
-      await fs.outputJson('./db/claim.json', record)
+      // await fs.outputJson('./db/claim.json', record)
       return process.exit(0)
     } else if (argv.job === 'collect') {
       const record = await Promise.map(
@@ -77,70 +93,55 @@
           const balance = await ronin_handler.get_balance(wallet.address, 'slp')
           console.log({ i: i + 1, slp: balance })
           if (balance === 0) {
-            return { i: i + 1, slp: 0, pay: 0 }
+            return { i: i + 1, slp: 0 }
           }
           await ronin_handler.transfer_slp(wallet.address, wallet.privateKey, main_wallet.address, balance)
+          await sleep(200)
           return { i: i + 1, slp: balance }
         },
         { concurrency: 3 }
       )
-      await fs.outputJson('./db/collect.json', record)
+      // await fs.outputJson('./db/collect.json', record)
       return process.exit(0)
     } else if (argv.job === 'calcu') {
       const scholar_info = await fs.readJSON('./db/info.json')
       const collect = await fs.readJSON('./db/collect.json')
       const result = collect.map((data, i) => {
-        console.log(data, i, scholar_info[i])
         data.address = scholar_info[i].address
-        if (scholar_info[i].extra === true) {
-          data.extra = true
-          data.event = Math.round(data.slp * 0.5)
-        } else {
-          data.event = data.slp
-        }
-        if (scholar_info[i].extra === true && data.slp >= 5600) {
-          data.pay = Math.round(data.slp * 0.7)
-          data.share = 0.7
-        } else if (scholar_info[i].extra === true && data.slp >= 4480) {
-          data.pay = Math.round(data.slp * 0.6)
-          data.share = 0.6
-        } else if (!scholar_info[i].extra && data.slp >= 2800) {
-          data.pay = Math.round(data.slp * 0.7)
-          data.share = 0.7
-        } else if (!scholar_info[i].extra && data.slp >= 2240) {
-          data.pay = Math.round(data.slp * 0.6)
-          data.share = 0.6
-        } else if (data.slp >= 1680) {
-          data.pay = Math.round(data.slp * 0.5)
-          data.share = 0.5
-        } else if (data.slp >= 840) {
-          data.pay = data.slp - 840
-          data.share = 0
+        data.energy = scholar_info[i].energy
+        if (scholar_info[i].energy === 60 && data.slp >= 1050) {
+          data.pay = data.slp - 1050
+        } else if (scholar_info[i].energy === 60 && data.slp < 1050) {
+          data.pay = 0
+        } else if (scholar_info[i].energy === 40 && data.slp >= 700) {
+          data.pay = data.slp - 700
+        } else if (scholar_info[i].energy === 40 && data.slp < 700) {
+          data.pay = 0
+        } else if (data.slp >= 350) {
+          data.pay = data.slp - 350
+        } else if (data.slp < 350) {
+          data.pay = 0
         } else {
           data.pay = 0
-          data.share = 0
         }
-        return data
-      })
-      result.sort((a, b) => {
-        return b.event - a.event
-      })
-      ;[1000, 700, 500, 400, 400].map((award, i) => {
-        result[i].rank = i + 1
-        result[i].award = award
-        console.log(result[i])
-      })
-      result.sort((a, b) => {
-        return a.i - b.i
-      })
-      result.map((data) => {
-        delete data.event
+        if (scholar_info[i].reduce && data.slp > 25 * scholar_info[i].reduce) {
+          data.new_pay = data.slp - 25 * scholar_info[i].reduce
+        }
         return data
       })
       await fs.outputJson('./db/calcu.json', result)
       return process.exit(0)
+    } else if (argv.job === 'scatter') {
+      const calcu = await fs.readJSON('./db/calcu.json')
+      const result = calcu.reduce((s, e) => {
+        if (!e.address || (e.pay === 0 && !e.new_pay)) {
+          return s
+        }
+        return s + `${e.address},${e.new_pay ? e.new_pay : e.pay}\n`
+      }, '')
+      await fs.writeFile('./db/scatter.txt', result)
     } else if (argv.job === 'count') {
-      const collect = await fs.readJSON('./db/collect.json')
+      const collect = await fs.readJSON('./db/calcu.json')
       const count = collect.reduce(
         (count, e) => {
           count[0] += e.slp
@@ -153,21 +154,38 @@
       console.log({ total: count[0], manager: count[0] - count[1], scholar: count[1] })
       return process.exit(0)
     } else if (argv.job === 'pay') {
-      const collect = await fs.readJSON('./db/collect.json')
+      const collect = await fs.readJSON('./db/calcu.json')
       const scholar_info = await fs.readJSON('./db/info.json')
-      await Promise.mapSeries(collect, async (e) => {
-        if (e.pay === 0) {
-          return { i: e.i, slp: 0 }
-        }
-        if (!scholar_info[e.i - 1].address) {
-          return { i: e.i, slp: 0 }
-        }
-        const scholar_address = e.address.includes('ronin:')
-          ? e.address.replace('ronin:', '0x')
-          : scholar_info[e.address - 1].address.replace('ronin:', '0x')
-        const pay_total = e.pay + (e.award || 0)
-        await ronin_handler.transfer_slp(main_wallet.address, main_wallet.privateKey, scholar_address, pay_total)
-      })
+      await Promise.map(
+        collect,
+        async (e, i) => {
+          // if (e.pay === 0) {
+          //   return { i: e.i, slp: 0 }
+          // }
+          // if (!scholar_info[e.i - 1].address) {
+          //   return { i: e.i, slp: 0 }
+          // }
+          const wallet = ronin_handler.get_key(mnemonic, i + skip)
+          const balance = await ronin_handler.get_balance(wallet.address, 'slp')
+          if (balance === 0) {
+            return true
+          }
+          const scholar_address = e.address.replace('ronin:', '0x')
+          // const pay_total = e.pay + (e.award || 0)
+          console.log(i, scholar_address, e.pay)
+          await ronin_handler.transfer_slp(wallet.address, wallet.privateKey, main_wallet.address, balance)
+          // await ronin_handler.transfer_slp(wallet.address, wallet.privateKey, scholar_address, e.pay)
+          // await ronin_handler.transfer_slp(main_wallet.address, main_wallet.privateKey, scholar_address, pay_total)
+        },
+        { concurrency: 3 }
+      )
+      return process.exit(0)
+    } else if (argv.job === 'ron') {
+      const result = [...Array(total)].reduce((s, e, i) => {
+        const wallet = ronin_handler.get_key(mnemonic, i + skip)
+        return s + `${wallet.address.replace('0x', 'ronin:')},0.1\n`
+      }, '')
+      await fs.writeFile('./db/ron.txt', result)
       return process.exit(0)
     }
   } catch (err) {
